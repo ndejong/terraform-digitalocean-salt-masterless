d19294b5c9a6aaa8f6f5d26dbee17ecf1090f985
Nicholas de Jong <contact@nicholasdejong.com>
2018-07-18 16:46:40 +0800

    adjust for /bin/sh

--- data/cloudinit-bootstrap.sh
+++ data/cloudinit-bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Copyright (c) 2018 Verb Networks Pty Ltd <contact [at] verbnetworks.com>
 #  - All rights reserved.
@@ -7,12 +7,12 @@
 #  - http://www.apache.org/licenses/LICENSE-2.0
 
 # volume0 - if defined
-if [ `echo -n ${volume0_dev} | wc -c` -gt 8 ]; then
+if [ $(echo -n ${volume0_dev} | wc -c) -gt 8 ]; then
     while [ ! -b ${volume0_dev} ];
         do sleep 1;
     done;
-    if [ `cat /etc/fstab | grep "${volume0_mount}" | wc -l` -lt 1 ]; then
-        echo `lsblk --fs --output UUID --noheadings --pairs ${volume0_dev}`' '${volume0_mount}' '${volume0_fstype}' defaults 0 2' >> /etc/fstab
+    if [ $(cat /etc/fstab | grep "${volume0_mount}" | wc -l) -lt 1 ]; then
+        echo $(lsblk --fs --output UUID --noheadings --pairs ${volume0_dev})' '${volume0_mount}' '${volume0_fstype}' defaults 0 2' >> /etc/fstab
     fi
     mount --all
 fi;
